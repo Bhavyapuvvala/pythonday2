c0bc934c2db745b465027dcbdabe114b27c84448
Bhavyapuvvala <bhavyapuvvala373@gmail.com>
2025-06-20 12:37:39 +0530

    Update operators.py

---
+++ operators.py
@@ -0,0 +1,14 @@
+""""
+An operator is a special symbol which is used to perfrom an operation
+eg: c=a+b 
+here c, a ,b are operands
++ is a special symbol
+The python operators are
+1. Arthimetic Operator
+2. Realtional Operator
+3. Logical Operator
+4. Bitwise Operator
+5. Identity Operator
+6. Membership Operator
+7. Assignment Operator
+"""
